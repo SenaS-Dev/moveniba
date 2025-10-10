3f137980458fef68f3b6541da872f3f30b7dd003
moveniba <cesarhenriquesena3@gmail.com>
2025-10-10 14:38:28 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/moveniba/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.moveniba;
+
+public class QuestionAnswer {
+    public static String question[] ={
+                "Qual a cor da banana?",
+                "Quanto é 2+2?",
+                "quantos lados tem um hexagono"
+    };
+
+    public static String choices[][] = {
+            {"Amarela","Azul","Verde","Vermelho"},
+            {"5","3","4","6"},
+            {"7","8","5","6"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Amarela",
+            "4",
+            "6"
+
+    };
+
+
+
+
+
+}
